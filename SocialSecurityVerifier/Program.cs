be52fe39bcbd03db91dbd63d1c377d3c58fcfae3
Mark Munson <mmunson@autopayments.com>
2016-04-09 13:11:35 -0700

    Finished program

--- SocialSecurityVerifier/Program.cs
+++ SocialSecurityVerifier/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SocialSecurityVerifier
 {
@@ -10,6 +7,27 @@ namespace SocialSecurityVerifier
     {
         static void Main(string[] args)
         {
+            var ssn = "555-12-3333";
+            DisplayResults(TestSocialSecurityNumber(ssn), ssn);
+
+            ssn = "fake ssn";
+            DisplayResults(TestSocialSecurityNumber(ssn), ssn);
+
+            Console.ReadKey();
+        }
+
+        static void DisplayResults(bool result, string ssn)
+        {
+            if (result)
+                Console.WriteLine(string.Format("SSN '{0}' is valid.", ssn));
+            else
+                Console.WriteLine(string.Format("SSN '{0}' is NOT valid.", ssn));
+        }
+
+        static bool TestSocialSecurityNumber(string ssn)
+        {
+            //Using SSN regex from http://www.regexlib.com/Search.aspx?k=social%20security%20number by Author Steven Smith
+            return Regex.Match(ssn, @"^\d{3}-\d{2}-\d{4}$").Success;
         }
     }
 }
